6c328a082906ceaad9912a2bf4c4e9a3eae82399
Paola Rivas Arana <drivasa3@miumg.edu.gt>
2023-10-17 22:19:28 -0600

    primer commit

---
+++ app/src/main/java/com/example/mascotafeliz/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.mascotafeliz.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Orange = Color(0xFFff9800)
+val OrangeLight = Color(0xFFffc947)
+val OrangeDark = Color(0xFFc66900)
+
+val BlueGrey = Color(0xFF78909c)
+val BlueGreyLight = Color(0xFFa7c8cd)
+val BlueGreyDark = Color(0xFF4b636e)
